d99e79b70cbc3a8e5be22de11f618a9ace698245
ShendadaAllen <48463152+ShendadaAllen@users.noreply.github.com>
2019-03-17 09:59:03 +0800

    模块
    模块

---
+++ shen-35体验模块.py
@@ -0,0 +1,3 @@
+import  shen_分割线模块
+shen_分割线模块.print_line('*', 50)
+shen_分割线模块.print_lines('-', 50)
---
+++ shen_34分割线模块.py
@@ -0,0 +1,10 @@
+def print_line(char, times):
+    print(char*times)
+def print_lines(char, times):
+    i = 1
+    while i <= 5:
+        print(char*times)
+        i += 1
+
+
+
