2192abd1fddbc5323e39fdc146e8b7567ca3efd7
ShendadaAllen <48463152+ShendadaAllen@users.noreply.github.com>
2019-03-17 09:35:52 +0800

    循环
    循环

---
+++ shen_18_0-100之间偶数之和.py
@@ -0,0 +1,13 @@
+# i = 0
+# sum_i = 0
+# while i <= 100:
+#     sum_i += i
+#     i += 2
+# print(sum_i)
+i = 0
+sum_i = 0
+while i <= 100:
+    if i % 2 == 0:
+        sum_i += i
+    i += 1
+print(sum_i)
