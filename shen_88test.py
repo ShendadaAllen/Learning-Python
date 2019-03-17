3f0cfd652ca6b2df99ad01881842672b92b9a225
ShendadaAllen <48463152+ShendadaAllen@users.noreply.github.com>
2019-03-17 13:51:17 +0800

    模块
    模块

---
+++ shen_88test.py
@@ -0,0 +1,6 @@
+def say_hello():
+    print("这是一个测试模块")
+    print("hello world")
+if __name__ == "__main__":
+    print("小明开发的模块")
+    say_hello()
---
+++ shen_89对于__name__.py
@@ -0,0 +1,5 @@
+import shen_test
+
+print("*" * 50)
+
+shen_test.say_hello()
